2846a5f6c4131a64deda91bf80f969ab2813b169
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-01-20 09:54:28 +0100

    Make print statement around mainCommand call

--- pseudolang.py
+++ pseudolang.py
@@ -8,4 +8,4 @@ class Pseudolang(engine.Engine):
     cmds = commands.Commands()
 
     def executeCommand(self, cmd):
-        self.cmds.mainCommand([cmd])
+        print(self.cmds.mainCommand([cmd]))
