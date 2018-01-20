ffa82ef9540dea106379427d32571f0bf4813b0b
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-01-20 09:46:29 +0100

    Added test main class

---
+++ pseudolang.py
@@ -0,0 +1,11 @@
+import engine
+import parser
+import reader
+import commands
+
+class Pseudolang(engine.Engine):
+
+    cmds = commands.Commands()
+
+    def executeCommand(self, cmd):
+        cmds.mainCommands(["v"])
